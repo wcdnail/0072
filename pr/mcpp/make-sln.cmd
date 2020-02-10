be324f3f48d2354d45cca1ef12b1526f0bf835ee
Michael Olegovich <miken@datasunrise.com>
2020-02-10 13:59:02 +0300

    ++

---
+++ pr/mcpp/make-sln.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+set VSBLD=build-vc
+
+mkdir %VSBLD%
+cd %VSBLD%
+
+rem -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%/scripts/buildsystems/vcpkg.cmake" ^
+
+@echo on
+cmake ^
+-DCMAKE_BUILD_TYPE=Debug ^
+-G "Visual Studio 15 2017 Win64" ..
+@echo off
+cd ..
+
