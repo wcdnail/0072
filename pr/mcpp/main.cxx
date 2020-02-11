eb8ba9ca63c11bcdab1abebd79accd050114bf89
Михаил Олегович <wcdnail@gmail.com>
2020-02-11 06:09:41 +0300

    ++

--- pr/mcpp/main.cxx
+++ pr/mcpp/main.cxx
@@ -1,11 +1,24 @@
 #include "dumps.hxx"
 #include <boost/config.hpp>
 #include <boost/version.hpp>
+#include <boost/type_index.hpp>
+#include <type_traits>
+#include <limits>
 
 void template_type_deduction();
 
+struct Base {
+    void f(int) { std::cout << "i"; }
+};
+
+struct Derived : Base {
+    void f(double) { std::cout << "d"; }
+};
+
 int main()
 {
+    using boost::typeindex::type_id_with_cvr;
+
     DMP_SEP();
     std::cout << "Build host : " << BOOST_PLATFORM << "\n"
               << "Compiler   : " << BOOST_COMPILER << "\n"
@@ -13,6 +26,9 @@ int main()
               << "Boost v.   : " << BOOST_VERSION << "\n"
               ;
     DMP_SEP();
-    template_type_deduction();
+    Derived d;
+    int i = 0;
+    d.f(i);
+    //template_type_deduction();
     return 0;
 }
