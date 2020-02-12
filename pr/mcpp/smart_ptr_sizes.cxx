a5c780000ffeace35d1d84234c3c4b69dfe4442f
Михаил Олегович <wcdnail@gmail.com>
2020-02-12 07:20:52 +0300

    +++++++

--- pr/mcpp/smart_ptr_sizes.cxx
+++ pr/mcpp/smart_ptr_sizes.cxx
@@ -11,7 +11,6 @@ struct Obj
 {
     int           id{ 0 };
     std::string str1{ "str1" };
-    std::string str2{ "str2" };
 
     Obj(int n = -1) : id(n) { std::cout << "C" << id << "\n"; }
     Obj(Obj&&) { std::cout << "RR" << id << "\n"; }
@@ -33,18 +32,29 @@ static void objectDeleter(Obj *obj)
 
 void unique_ptr_size_test()
 {
-    uint64_t lamdaDelCount = 0;
-    auto lamdaDel = [&lamdaDelCount](Obj *obj) {
+    auto lambdaDel = [](Obj *obj) {
         delete obj;
-        ++lamdaDelCount;
     };
-    using LamdaDel = decltype(lamdaDel);
+    using LambdaDel = decltype(lambdaDel);
+
+    uint64_t lambdaDelCount = 0;
+    auto lambdaDelComp = [&lambdaDelCount](Obj *obj) {
+        delete obj;
+        ++lambdaDelCount;
+    };
+    using LambdaDelComp = decltype(lambdaDelComp);
 
     struct ObjDel
     {
-        uint64_t count{ 0 };
-        char      sign{ 'a' };
+        void operator() (Obj *obj) noexcept
+        {
+            delete obj;
+        }
+    };
 
+    struct ObjDelComp
+    {
+        uint64_t count{ 0 };
         void operator() (Obj *obj) noexcept
         {
             delete obj;
@@ -52,22 +62,29 @@ void unique_ptr_size_test()
         }
     };
 
-    std::cout << "LambdaDel : " << DMP_DECLTYPE_NAME(lamdaDel) << "\n";
+    std::cout << "LambdaDel : " << DMP_DECLTYPE_NAME(lambdaDel) << "\n";
 
     DMP_EXPR1(sizeof(Obj));
     DMP_EXPR1(sizeof(Obj*));
-    DMP_EXPR1(sizeof(LamdaDel));
-    DMP_EXPR1(sizeof(lamdaDel));
+    DMP_EXPR1(sizeof(LambdaDel));
+    DMP_EXPR1(sizeof(lambdaDel));
+    DMP_EXPR1(sizeof(ObjDel));
+    DMP_EXPR1(sizeof(LambdaDelComp));
+    DMP_EXPR1(sizeof(lambdaDelComp));
+    DMP_EXPR1(sizeof(ObjDelComp));
     DMP_EXPR1(sizeof(std::unique_ptr<Obj>));
     DMP_EXPR1(sizeof(std::unique_ptr<Obj, ObjDel>));
+    DMP_EXPR1(sizeof(std::unique_ptr<Obj, LambdaDel>));
+    DMP_EXPR1(sizeof(std::unique_ptr<Obj, ObjDelComp>));
+    DMP_EXPR1(sizeof(std::unique_ptr<Obj, LambdaDelComp>));
     DMP_EXPR1(sizeof(std::unique_ptr<Obj, void(*)(Obj*)>));
     DMP_EXPR1(sizeof(std::unique_ptr<Obj, std::function<void(Obj*)>>));
-    DMP_EXPR1(sizeof(std::unique_ptr<Obj, LamdaDel>));
 
     ObjDel customDeleter;
     std::unique_ptr<Obj, ObjDel> customDelObj(new Obj(1), customDeleter);
-    std::unique_ptr<Obj, void(*)(Obj*)> funcDelObj(new Obj(2), objectDeleter);
-    std::unique_ptr<Obj, std::function<void(Obj*)>> funcDelObj2(new Obj(3), objectDeleter);
-    std::unique_ptr<Obj, LamdaDel> lambdaDelObj(new Obj(4), lamdaDel);
+    std::unique_ptr<Obj, LambdaDel> lambdaDelObj(new Obj(2), lambdaDel);
+    std::unique_ptr<Obj, void(*)(Obj*)> funcDelObj(new Obj(3), objectDeleter);
+    std::unique_ptr<Obj, std::function<void(Obj*)>> funcDelObj2(new Obj(4), objectDeleter);
+
     std::cout << std::endl;
 }
